140655ab3874a250cb3c9a4e253a94e70af9cffc
Kwonnamhyung <rnjsskagud@gmail.com>
2023-01-19 20:38:47 +0900

    커뮤니티 entity 설계

--- interview-project/src/main/java/interview/interviewproject/Community/domain/Community.java
+++ interview-project/src/main/java/interview/interviewproject/Community/domain/Community.java
@@ -1,4 +1,29 @@
 package interview.interviewproject.Community.domain;
 
+import interview.interviewproject.Member.domain.Member;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Community {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String contents;
+
+    private CommunityType category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "community" , cascade = CascadeType.ALL)
+    private List<CommunityTag> communityTagList = new ArrayList<>();
+
 }
--- interview-project/src/main/java/interview/interviewproject/Community/domain/CommunityTag.java
+++ interview-project/src/main/java/interview/interviewproject/Community/domain/CommunityTag.java
@@ -1,4 +1,18 @@
 package interview.interviewproject.Community.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class CommunityTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tagName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "community_id")
+    private Community community;
+
 }
