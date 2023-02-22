fb4eb171a6db6093a1b987d27d4fce6173a033b1
Kwonnamhyung <rnjsskagud@gmail.com>
2023-02-22 14:06:34 +0900

    test

--- interview-project/src/main/java/interview/interviewproject/Member/controller/MemberController.java
+++ interview-project/src/main/java/interview/interviewproject/Member/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 @RequestMapping("/api/v1/member")
 public class MemberController {
 
