720b6c6cecfbce652ecbc8b0c8546b0538ae5cee
yuseok0215 <elwlahstmxjf@naver.com>
2023-03-08 18:19:27 +0900

    filter로 변경

--- interview-project/src/main/java/interview/interviewproject/Member/controller/MemberController.java
+++ interview-project/src/main/java/interview/interviewproject/Member/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/member")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class MemberController {
 
     private final MemberService memberService;
