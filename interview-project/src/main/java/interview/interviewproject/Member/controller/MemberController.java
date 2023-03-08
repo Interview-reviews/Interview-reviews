dd6e8d70f65293f3658f12c5cdd5160cb9babae2
yuseok0215 <elwlahstmxjf@naver.com>
2023-03-08 20:01:06 +0900

    수정

--- interview-project/src/main/java/interview/interviewproject/Member/controller/MemberController.java
+++ interview-project/src/main/java/interview/interviewproject/Member/controller/MemberController.java
@@ -62,8 +62,8 @@ public class MemberController {
         return emailService.findPw(email);
     }
 
-    @GetMapping("/test")
-    public String test(){
-        return "test";
-    }
+//    @GetMapping("/test")
+//    public String test(){
+//        return "test";
+//    }
 }
